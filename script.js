3d91c6ab6b55ccffc1f392b6ab3640e3225fc2a0
Mathis-Philippe <mathis.philippe@my-digital-school.org>
2024-10-30 16:31:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const loader = document.querySelector('.loader');
+
+window.addEventListener('load', () => {
+
+    setTimeout(() => {
+        loader.classList.add('fondu_out');
+    },1000)
+
+    
+
+})

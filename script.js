e2761877b3d0937eee30d276b7da718cd29165dc
Mathis-Philippe <mathis.philippe@my-digital-school.org>
2024-11-06 10:20:12 +0100

    hover and menu burger"
    "
    git add .

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ window.addEventListener('load', () => {
     
 
 })
+
+const sections = document.querySelectorAll('.section');
+
+window.addEventListener('scroll', () => {
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const screenBottom = window.innerHeight;
+        
+        if (sectionTop < screenBottom) {
+            section.classList.add('visible');
+        }
+    });
+});
+function toggleMenu() {
+    const mobileMenu = document.querySelector('.menu_navbar_mobile');
+    mobileMenu.style.display = mobileMenu.style.display === 'flex' ? 'none' : 'flex';
+}
